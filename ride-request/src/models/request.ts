2b6a71e69aa7bab5d99d9e442c28ef5ea7e59db8
NeverLost-Never <hackinforlife@gmail.com>
2022-09-20 02:24:37 +0400

    Ride-Request - Models (request.ts)

---
+++ ride-request/src/models/request.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+interface RequestAttr {
+	pickUpPoint: {
+		lat: string;
+		lng: string;
+	};
+	destination: {
+		lat: string;
+		lng: string;
+	};
+	timeOfDeparture: Date;
+}
+
+export interface RequestDoc extends RequestAttr, mongoose.Document {
+	version: number;
+	createdAt: Date;
+}
+
+interface RequestModel extends mongoose.Model<RequestDoc> {
+	build(attr: RequestAttr): RequestDoc;
+}
+
+const requestSchema = new mongoose.Schema(
+	{
+		pickUpPoint: { type: Object, required: true },
+		destination: { type: Object, required: true },
+		timeOfDeparture: { type: Date, required: true },
+		createdAt: { type: Date, required: true, default: Date.now },
+	},
+	{
+		toJSON: {
+			transform(doc, ret) {
+				ret.id = ret._id;
+				delete ret._id;
+				delete ret.__v;
+			},
+		},
+		timestamps: { createdAt: false },
+	}
+);
+
+requestSchema.index({ email: 1 });
+requestSchema.set("versionKey", "version");
+
+requestSchema.statics.build = (attrs: RequestAttr) => {
+	return new Request(attrs);
+};
+
+const Request = mongoose.model<RequestDoc, RequestModel>(
+	"Request",
+	requestSchema
+);
+
+export { Request };
