753496286d0dede01abd547850531b8a365da4b6
NeverLost-Never <hackinforlife@gmail.com>
2022-09-20 01:49:00 +0400

    Ride-Request Service - Setup

---
+++ ride-request/src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+// importing error-types and middlewares
+import { errorHandler, NotFoundError } from "@kmalae.ltd/library";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+	cookieSession({
+		signed: false,
+		secure: process.env.NODE_ENV !== "test",
+	})
+);
+
+app.all("*", async (req, res, next) => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+app.listen(3000, async () => {
+	if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
+
+	if (!process.env.MONGO_URL) throw new Error("MONGO_URI must be defined");
+
+	try {
+		await mongoose.connect(process.env.MONGO_URL);
+		console.log("connected to mongodb");
+	} catch (error) {
+		console.error(error);
+	}
+
+	console.log("listening on port 3000");
+});
