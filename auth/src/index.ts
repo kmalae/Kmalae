868e2b84be438fa8c7d78c48fb7c0e62f55dd2f6
Haileab-TG <haileab.t.g@gmail.com>
2022-09-25 14:24:55 +0400

    Authentication - (index.ts) splitted into app.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,46 +1,43 @@
-import { app } from "./app";
-import mongoose from "mongoose";
-import { natsWrapper } from "./nats-wrapper";
+import mongoose from 'mongoose';
+import { natsWrapper } from './nats-wrapper';
+import { app } from './app';
 
 app.listen(3000, async () => {
-	
-	if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
+	if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
 
-	if (!process.env.MONGO_URL) throw new Error("MONGO_URI must be defined");
+	if (!process.env.MONGO_URL) throw new Error('MONGO_URI must be defined');
 
 	if (!process.env.NATS_CLIENT_ID) {
-		throw new Error("NATS_CLIENT_ID must be defined");
+		throw new Error('NATS_CLIENT_ID must be defined');
 	}
 
 	if (!process.env.NATS_URL) {
-		throw new Error("NATS_URL must be defined");
+		throw new Error('NATS_URL must be defined');
 	}
 
 	if (!process.env.NATS_CLUSTER_ID) {
-		throw new Error("NATS_CLUSTER_ID must be defined");
+		throw new Error('NATS_CLUSTER_ID must be defined');
 	}
 
 	try {
 		await mongoose.connect(process.env.MONGO_URL);
-		console.log("connected to mongodb");
-
+		console.log('connected to mongodb');
 		await natsWrapper.connect(
 			process.env.NATS_CLUSTER_ID,
 			process.env.NATS_CLIENT_ID,
 			process.env.NATS_URL
 		);
 
-		natsWrapper.client.on("close", () => {
-			console.log("NATS connection closed!");
+		natsWrapper.client.on('close', () => {
+			console.log('NATS connection closed!');
 			process.exit();
 		});
 
-		process.on("SIGINT", () => natsWrapper.client.close());
-		process.on("SIGTERM", () => natsWrapper.client.close());
-
+		process.on('SIGINT', () => natsWrapper.client.close());
+		process.on('SIGTERM', () => natsWrapper.client.close());
 	} catch (error) {
 		console.error(error);
 	}
 
-	console.log("listening on port 3000");
+	console.log('listening on port 3000');
 });
