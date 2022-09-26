903b6dbf6c9749940eedcb8fad731d738505e793
Haileab-TG <haileab.t.g@gmail.com>
2022-09-26 19:26:11 +0400

    Recomm - (app.ts) routes/lift-request hooked

--- recommendation/src/app.ts
+++ recommendation/src/app.ts
@@ -6,6 +6,13 @@ import cookieSession from 'cookie-session';
 // importing error-types and middlewares
 import { errorHandler, NotFoundError } from '@kmalae.ltd/library';
 
+// importing routers
+import { createLiftRequestRouter } from './routes/lift-request/create-lift-request';
+import { getLiftRequestRouter } from './routes/lift-request/get-lift-request';
+import { getUserLiftRequestsRouter } from './routes/lift-request/get-user-lift-requests';
+import { updateLiftRequestRouter } from './routes/lift-request/update-lift-request';
+import { cancelLiftRequestRouter } from './routes/lift-request/cancel-lift-request';
+
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -15,6 +22,12 @@ app.use(
 		secure: process.env.NODE_ENV !== 'test',
 	})
 );
+
+app.use(createLiftRequestRouter);
+app.use(getLiftRequestRouter);
+app.use(getUserLiftRequestsRouter);
+app.use(updateLiftRequestRouter);
+app.use(cancelLiftRequestRouter);
 
 app.all('*', async (req, res, next) => {
 	throw new NotFoundError();
