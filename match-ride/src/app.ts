2f6fdc4b89d2de7ace4055297ff251c8847f044e
NeverLost-Never <hackinforlife@gmail.com>
2022-09-30 03:58:40 +0400

    MatchRide - Events (match-ride-confirmed-publisher.ts) (finished), and (app.ts) confirmMatchRequestRouter hooked

--- match-ride/src/app.ts
+++ match-ride/src/app.ts
@@ -1,31 +1,33 @@
-import express from 'express';
-import { json } from 'body-parser';
-import 'express-async-errors';
-import cookieSession from 'cookie-session';
+import express from "express";
+import { json } from "body-parser";
+import "express-async-errors";
+import cookieSession from "cookie-session";
 
 // importing error-types and middlewares
-import { errorHandler, NotFoundError } from '@kmalae.ltd/library';
+import { errorHandler, NotFoundError } from "@kmalae.ltd/library";
 
 // importing routers
-import { createMatchRequestRouter } from './routes/create-match-request';
-import { cancelMatchRequestRouter } from './routes/driver-cancel-request';
-import { passengerCancelledRequestRouter } from './routes/passenger-cancelled-request';
+import { createMatchRequestRouter } from "./routes/create-match-request";
+import { cancelMatchRequestRouter } from "./routes/driver-cancel-request";
+import { passengerCancelledRequestRouter } from "./routes/passenger-cancelled-request";
+import { confirmMatchRequestRouter } from "./routes/passenger-confirm-request";
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(json());
 app.use(
 	cookieSession({
 		signed: false,
-		secure: process.env.NODE_ENV !== 'test',
+		secure: process.env.NODE_ENV !== "test",
 	})
 );
 
 app.use(createMatchRequestRouter);
 app.use(cancelMatchRequestRouter);
 app.use(passengerCancelledRequestRouter);
+app.use(confirmMatchRequestRouter);
 
-app.all('*', async (req, res, next) => {
+app.all("*", async (req, res, next) => {
 	throw new NotFoundError();
 });
 
