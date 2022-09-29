31a351be28514b094a34ced4834a3e2b3457e7db
NeverLost-Never <hackinforlife@gmail.com>
2022-09-29 21:12:52 +0400

    MatchRide Service - Setup

---
+++ match-ride/src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { json } from "body-parser";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+// importing error-types and middlewares
+import { errorHandler, NotFoundError } from "@kmalae.ltd/library";
+
+// importing routers
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+	cookieSession({
+		signed: false,
+		secure: process.env.NODE_ENV !== "test",
+	})
+);
+
+app.all("*", async (req, res, next) => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ match-ride/src/index.ts
@@ -0,0 +1,73 @@
+import { natsWrapper } from "@kmalae.ltd/library";
+import mongoose from "mongoose";
+import { app } from "./app";
+
+// importing User event listeners
+import { UserRegisteredListener } from "./events/listen/user/user-registered-listener";
+import { UserUpdatedListener } from "./events/listen/user/user-updated-listener";
+
+// importing Vehicle event listeners
+import { VehicleRegisteredListener } from "./events/listen/vehicle/vehicle-registered-listener";
+import { VehicleUpdatedListener } from "./events/listen/vehicle/vehicle-updated-listener";
+import { VehicleDeletedListener } from "./events/listen/vehicle/vehicle-deleted-listener";
+
+// importing Ride-Request event listeners
+import { RideRequestCreatedListener } from "./events/listen/ride-request-listener/ride-request-created-listener";
+import { RideRequestUpdatedListener } from "./events/listen/ride-request-listener/ride-request-updated-listener";
+import { RideRequestCancelledListener } from "./events/listen/ride-request-listener/ride-request-cancelled-listener";
+
+app.listen(3000, async () => {
+	if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
+
+	if (!process.env.MONGO_URL) throw new Error("MONGO_URI must be defined");
+
+	if (!process.env.NATS_CLIENT_ID) {
+		throw new Error("NATS_CLIENT_ID must be defined");
+	}
+
+	if (!process.env.NATS_URL) {
+		throw new Error("NATS_URL must be defined");
+	}
+
+	if (!process.env.NATS_CLUSTER_ID) {
+		throw new Error("NATS_CLUSTER_ID must be defined");
+	}
+
+	try {
+		await mongoose.connect(process.env.MONGO_URL, {
+			dbName: "Recommendation-DB",
+		});
+		console.log("connected to mongodb");
+		await natsWrapper.connect(
+			process.env.NATS_CLUSTER_ID,
+			process.env.NATS_CLIENT_ID,
+			process.env.NATS_URL
+		);
+
+		natsWrapper.client.on("close", () => {
+			console.log("NATS connection closed!");
+			process.exit();
+		});
+
+		process.on("SIGINT", () => natsWrapper.client.close());
+		process.on("SIGTERM", () => natsWrapper.client.close());
+
+		// Listenting to User changes
+		new UserRegisteredListener(natsWrapper.client).listen();
+		new UserUpdatedListener(natsWrapper.client).listen();
+
+		// Listening to Vehicle changes
+		new VehicleRegisteredListener(natsWrapper.client).listen();
+		new VehicleUpdatedListener(natsWrapper.client).listen();
+		new VehicleDeletedListener(natsWrapper.client).listen();
+
+		// Listening to Ride-Request changes
+		new RideRequestCreatedListener(natsWrapper.client).listen();
+		new RideRequestUpdatedListener(natsWrapper.client).listen();
+		new RideRequestCancelledListener(natsWrapper.client).listen();
+	} catch (error) {
+		console.error(error);
+	}
+
+	console.log("listening on port 3000");
+});
