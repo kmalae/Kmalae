79f3a1f59bddf3f4a24c72869360bede8e8dbab9
NeverLost-Never <hackinforlife@gmail.com>
2022-09-30 18:58:29 +0400

    Review Service - Setup

---
+++ review/src/index.ts
@@ -0,0 +1,88 @@
+import { natsWrapper } from "@kmalae.ltd/library";
+import mongoose from "mongoose";
+import { app } from "./app";
+
+// importing User event listeners
+// import { UserRegisteredListener } from "./events/listen/user/user-registered-listener";
+// import { UserUpdatedListener } from "./events/listen/user/user-updated-listener";
+
+// importing Vehicle event listeners
+// import { VehicleRegisteredListener } from "./events/listen/vehicle/vehicle-registered-listener";
+// import { VehicleUpdatedListener } from "./events/listen/vehicle/vehicle-updated-listener";
+// import { VehicleDeletedListener } from "./events/listen/vehicle/vehicle-deleted-listener";
+
+// importing Ride-Request event listeners
+// import { RideRequestCreatedListener } from "./events/listen/ride-request-listener/ride-request-created-listener";
+// import { RideRequestUpdatedListener } from "./events/listen/ride-request-listener/ride-request-updated-listener";
+// import { RideRequestCancelledListener } from "./events/listen/ride-request-listener/ride-request-cancelled-listener";
+
+// importing Lift-Request event listeners
+// import { LiftRequestCreatedListener } from "./events/listen/lift-request-listener/lift-request-created-listener";
+// import { LiftRequestUpdatedListener } from "./events/listen/lift-request-listener/lift-request-updated-listener";
+// import { LiftRequestCancelledListener } from "./events/listen/lift-request-listener/lift-request-cancelled-listener";
+
+app.listen(3000, async () => {
+	if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
+
+	if (!process.env.MONGO_URL) throw new Error("MONGO_URI must be defined");
+
+	if (!process.env.NATS_CLIENT_ID) {
+		throw new Error("NATS_CLIENT_ID must be defined");
+	}
+
+	if (!process.env.NATS_URL) {
+		throw new Error("NATS_URL must be defined");
+	}
+
+	if (!process.env.NATS_CLUSTER_ID) {
+		throw new Error("NATS_CLUSTER_ID must be defined");
+	}
+
+	try {
+		await mongoose.connect(process.env.MONGO_URL, {
+			dbName: "Review-DB",
+		});
+		console.log("connected to mongodb");
+		await natsWrapper.connect(
+			process.env.NATS_CLUSTER_ID,
+			process.env.NATS_CLIENT_ID,
+			process.env.NATS_URL
+		);
+
+		natsWrapper.client.on("close", () => {
+			console.log("NATS connection closed!");
+			process.exit();
+		});
+
+		process.on("SIGINT", () => natsWrapper.client.close());
+		process.on("SIGTERM", () => natsWrapper.client.close());
+
+		// Listenting to User changes
+		// new UserRegisteredListener(natsWrapper.client).listen();
+		// new UserUpdatedListener(natsWrapper.client).listen();
+
+		// Listening to Vehicle changes
+		// new VehicleRegisteredListener(natsWrapper.client).listen();
+		// new VehicleUpdatedListener(natsWrapper.client).listen();
+		// new VehicleDeletedListener(natsWrapper.client).listen();
+
+		// Listening to Ride-Request changes
+		// new RideRequestCreatedListener(natsWrapper.client).listen();
+		// new RideRequestUpdatedListener(natsWrapper.client).listen();
+		// new RideRequestCancelledListener(natsWrapper.client).listen();
+
+		// Listening to Ride-Request changes
+		// new LiftRequestCreatedListener(natsWrapper.client).listen();
+		// new LiftRequestUpdatedListener(natsWrapper.client).listen();
+		// new LiftRequestCancelledListener(natsWrapper.client).listen();
+
+		// Listening to Match-Ride changes
+		// new MatchRideCreatedListener(natsWrapper.client).listen();
+		// new MatchRideConfirmedListener(natsWrapper.client).listen();
+		// new MatchRideCancelledListener(natsWrapper.client).listen();
+	} catch (error) {
+		console.error(error);
+	}
+
+	console.log("listening on port 3000");
+});
---
+++ review/src/models/review.ts
@@ -0,0 +1,75 @@
+import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+
+interface ReviewAttr {
+	passenger: string;
+	driver: string;
+	ride: string;
+	passengerRated: number;
+	passengerCommented: string;
+	driverRated: number;
+	driverCommented: string;
+	version: number;
+}
+
+export interface ReviewDoc extends ReviewAttr, mongoose.Document {
+	version: number;
+}
+
+interface ReviewModel extends mongoose.Model<ReviewDoc> {
+	build(attr: ReviewAttr): ReviewDoc;
+}
+
+const reviewSchema = new mongoose.Schema(
+	{
+		passenger: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+		driver: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+		rideRequest: {
+			type: mongoose.Types.ObjectId,
+			required: true,
+			ref: "RideRequest",
+		},
+		liftRequest: {
+			type: mongoose.Types.ObjectId,
+			required: true,
+			ref: "LiftRequest",
+		},
+		matchRide: {
+			type: mongoose.Types.ObjectId,
+			required: true,
+			ref: "MatchRide",
+		},
+		passengerRated: { type: Number, required: true },
+		passengerCommented: { type: String, required: true },
+		driverRated: { type: Number, required: true },
+		driverCommented: { type: String, required: true },
+	},
+	{
+		toJSON: {
+			transform(doc, ret) {
+				ret.id = ret._id;
+				delete ret._id;
+				delete ret.__v;
+			},
+		},
+		timestamps: { createdAt: false },
+	}
+);
+
+reviewSchema.index({
+	passenger: 1,
+	driver: 1,
+	rideRequest: 1,
+	liftRequest: 1,
+	matchRide: 1,
+});
+reviewSchema.set("versionKey", "version");
+reviewSchema.plugin(updateIfCurrentPlugin);
+
+reviewSchema.statics.build = (attrs: ReviewAttr) => {
+	return new Review(attrs);
+};
+
+const Review = mongoose.model<ReviewDoc, ReviewModel>("Review", reviewSchema);
+
+export { Review };
