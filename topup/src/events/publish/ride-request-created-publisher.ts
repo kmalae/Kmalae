5ea7ae04d352781ecd0e7a616405095d02b4aa45
NeverLost-Never <hackinforlife@gmail.com>
2022-10-01 15:33:26 +0400

    Topup - Events - removed uneeded files

--- topup/src/events/publish/ride-request-created-publisher.ts
+++
@@ -1,9 +0,0 @@
-import {
-	Publisher,
-	Subjects,
-	RideRequestCreatedEvent,
-} from '@kmalae.ltd/library';
-
-export class RideRequestCreatdPublisher extends Publisher<RideRequestCreatedEvent> {
-	subject: Subjects.RideRequestCreated = Subjects.RideRequestCreated;
-}
--- topup/src/events/publish/ride-request-updated-publisher.ts
+++
@@ -1,9 +0,0 @@
-import {
-	Publisher,
-	Subjects,
-	RideRequestUpdatedEvent,
-} from '@kmalae.ltd/library';
-
-export class RideRequestUpdatedPublisher extends Publisher<RideRequestUpdatedEvent> {
-	subject: Subjects.RideRequestUpdated = Subjects.RideRequestUpdated;
-}
