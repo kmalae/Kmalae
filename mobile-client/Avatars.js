aa012029c6388dc20f20e9d7d776bfebffbdf252
NeverLost-Never <hackinforlife@gmail.com>
2022-11-21 22:14:44 +0400

    Mobile-Client - Datasets (Avatars.js and Vehicles.js) added

---
+++ mobile-client/Avatars.js
@@ -0,0 +1,34 @@
+export const AvatarImages = [
+	{
+		id: 1,
+		image: require("./assets/images/avatars/avatar_1.png"),
+	},
+	{
+		id: 2,
+		image: require("./assets/images/avatars/avatar_2.png"),
+	},
+	{
+		id: 3,
+		image: require("./assets/images/avatars/avatar_3.png"),
+	},
+	{
+		id: 4,
+		image: require("./assets/images/avatars/avatar_4.png"),
+	},
+	{
+		id: 5,
+		image: require("./assets/images/avatars/avatar_5.png"),
+	},
+	{
+		id: 6,
+		image: require("./assets/images/avatars/avatar_6.png"),
+	},
+	{
+		id: 7,
+		image: require("./assets/images/avatars/avatar_7.png"),
+	},
+	{
+		id: 8,
+		image: require("./assets/images/avatars/avatar_8.png"),
+	},
+];
